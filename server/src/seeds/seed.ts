741b109439254a1d75bef59930a9dba6b07dcad5
omgxlori <lbelovin@gmail.com>
2025-01-27 16:23:51 -0500

    Update seed.ts

--- server/src/seeds/seed.ts
+++ server/src/seeds/seed.ts
@@ -1,13 +1,18 @@
-const db = require("../config/connection.js");
-const Question = require("../models/Question.js");
-const cleanDB = require("./cleanDb.js");
-const pythonQuestions = require('./pythonQuestions.json');
+import db from "../config/connection.js";
+import Question from "../models/Question.js";
+import cleanDB from "./cleanDb.js";
+import pythonQuestions from "./pythonQuestions.json" assert { type: "json" };
 
-db.once('open', async () => {
-  await cleanDB('Question', 'questions');
+db.once("open", async () => {
+  try {
+    await cleanDB("Question", "questions");
 
-  await Question.insertMany(pythonQuestions);
+    await Question.insertMany(pythonQuestions);
 
-  console.log('Questions seeded!');
-  process.exit(0);
+    console.log("Questions seeded!");
+    process.exit(0);
+  } catch (error) {
+    console.error("Error seeding data:", error);
+    process.exit(1);
+  }
 });
