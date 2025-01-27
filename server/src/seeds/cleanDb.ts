d570f8401671a4d356dfba88cfe164dd68e7c5ac
omgxlori <lbelovin@gmail.com>
2025-01-27 17:26:01 -0500

    Update seed files for Render workflow

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -3,14 +3,21 @@ import db from '../config/connection.js';
 
 export default async (modelName: "Question", collectionName: string) => {
   try {
-    let modelExists = await models[modelName].db.db.listCollections({
-      name: collectionName
-    }).toArray()
+    if (
+      models[modelName]?.db?.db
+    ) {
+      const modelExists = await models[modelName].db.db.listCollections({
+        name: collectionName,
+      }).toArray();
 
-    if (modelExists.length) {
-      await db.dropCollection(collectionName);
+      if (modelExists.length > 0) {
+        await db.dropCollection(collectionName);
+      }
+    } else {
+      throw new Error(`Database or model not properly initialized for ${modelName}`);
     }
   } catch (err) {
+    console.error(`Error in dropping collection "${collectionName}":`, err);
     throw err;
   }
-}
+};
